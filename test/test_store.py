cf0a4f1d092e387b6bb26e06ead2afd964831fec
katemaximova <ekaterinamaximovaqa@gmail.com>
2025-11-01 15:36:26 +0300

    - внесены правки по PR от Димы - создан файл с json-schema для размещения заказа - создан файл с json-shema для получения инвентаря

--- test/test_store.py
+++ test/test_store.py
@@ -1,23 +1,38 @@
 import allure
 import pytest
 import requests
-
+import jsonschema
+from test.schemas.order_schema import ORDER_SCHEMA
+from test.schemas.inventory_schema import INVENTORY_SCHEMA
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
 @allure.feature("Store")
 class TestStore:
     @allure.title("проверка размещения заказа в магазине")
-    def test_place_a_new_order_in_the_store(self,create_order):
-            with allure.step("Отправка запроса на размещение заказа"):
-                response = requests.post(url=f"{BASE_URL}/store/order",json=create_order)
-                response_json = response.json()
-            with allure.step("Проверка статуса ответа и данных заказа"):
-                assert response.status_code == 200
-                assert response_json["id"] == create_order["id"]
-                assert response_json["petId"] == create_order["petId"]
-                assert response_json["quantity"] == create_order["quantity"]
-                assert response_json["status"] == create_order["status"]
-                assert response_json["complete"] == create_order["complete"]
+    def test_place_a_new_order_in_the_store(self):
+        with allure.step("Подготовка данных для размещения заказа"):
+            payload = {
+                "id": 1,
+                "petId": 1,
+                "quantity": 1,
+                "status": "placed",
+                "complete": True
+            }
+
+        with allure.step("Отправка запроса на размещение заказа"):
+            response = requests.post(url=f"{BASE_URL}/store/order", json=payload)
+            response_json = response.json()
+
+        with allure.step("Проверка статус-кода и структуры ответа"):
+            assert response.status_code == 200
+            jsonschema.validate(instance=response_json, schema=ORDER_SCHEMA)
+
+        with allure.step("Проверка корректности данных заказа в ответе"):
+            assert response_json["id"] == payload["id"]
+            assert response_json["petId"] == payload["petId"]
+            assert response_json["quantity"] == payload["quantity"]
+            assert response_json["status"] == payload["status"]
+            assert response_json["complete"] == payload["complete"]
 
     @allure.title("Получение информации о заказе по ID")
     def test_get_info_by_id (self,create_order):
@@ -56,18 +71,17 @@ class TestStore:
 
 
     @allure.title("Получение инвентаря магазина")
-    @pytest.mark.parametrize(
-        "expected_data, expected_status_code",
-        [
-            ({"approved": 57, "delivered": 50}, 200) #в Swagger в примере эндпоинта данных больше чем в требованиях самого теста, если это так и должно быть - ОК,если инфа устарела - я добавлю...ну либо я что - то не так поняла как делать +)))
-        ]
-    )
-    def test_get_inventory(self,expected_data, expected_status_code):
-        with allure.step("Запрос на получение инвентаря"):
-             response = requests.get(url=f"{BASE_URL}/store/inventory")
-        with allure.step("Проверка статуса ответа и формата данных"):
-            assert response.status_code == expected_status_code
-            assert isinstance(response.json(), dict)
+    def test_get_store_inventory(self):
+        with allure.step("отправка запроса на получение инвентаря"):
+            response = requests.get(url=f"{BASE_URL}/store/inventory")
+            response_json = response.json()
+
+        with allure.step("проверка ответа и валидация JSON-схемы"):
+            assert response.status_code == 200
+            jsonschema.validate(instance=response_json, schema=INVENTORY_SCHEMA)
+
+
+
 
 
 
